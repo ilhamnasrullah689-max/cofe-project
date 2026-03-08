3940f1687c2956bb06bb3db39252b3f5d609f479
Muhammad Ilham Nasrullah <ilhamnasrullah689@gmail.com>
2026-03-08 17:21:04 +0700

    Fix: Menu routing and footer mobile layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./components/LandingPage";
+import LandingPage from "./components/LandingPage"; // Sesuaikan folder jika perlu
+import Menu from "./Menu"; // Sesuai hasil 'ls src' kamu
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        {/* Jika ada halaman menu, tambahkan di sini nanti */}
+        <Route path="/menu" element={<Menu />} />
       </Routes>
     </Router>
   );
